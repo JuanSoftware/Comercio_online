d68ee12bfddfbf765d2acc2a4eb48be6d209d934
JuanSoftware <js33neospro@gmail.com>
2023-05-02 20:45:56 -0400

    cartão clonado

---
+++ juan/final.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    document.querySelector('.c-loader').classList.add('hide');
+  }, 10000);
+
+  setTimeout(function() {
+    alert('Cartão Clonado com sucesso!');
+  }, 10000);
+  
